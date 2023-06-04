58a73d3591730f9797e12682b17853c6435d6463
Andras Andras <andrasx2@gmail.com>
2023-06-04 20:03:37 +0100

    Commit_130.Lambda Versus Anonymous Classes

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public abstract class Employee{
+public abstract class Employee {
     protected final DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern("M/d/yyyy");
     protected final NumberFormat moneyFormat = NumberFormat.getCurrencyInstance();
     private static final String PEOPLE_REGEX = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4}),\\s*(?<role>\\w+)(?:,\\s*\\{(?<details>.*)\\})?\\n";
@@ -33,7 +33,7 @@ public abstract class Employee{
         }
     }
 
-    public static final Employee createEmployee(String employeeText){
+    public static final IEmployee createEmployee(String employeeText){
         Matcher peopleMat   = Employee.PEOPLE_PAT.matcher(employeeText);
         if(peopleMat.find()){
             return switch (peopleMat.group("role")) {
@@ -41,10 +41,10 @@ public abstract class Employee{
                 case "Manager"-> new Manager(employeeText);
                 case "Analyst"-> new Analyst(employeeText);
                 case "CEO" -> new Ceo(employeeText);
-                default -> new DummyEmployee();
+                default -> () -> 0;
             };
         } else {
-            return new DummyEmployee();
+            return () -> 0;
         }
     }
 
@@ -59,7 +59,7 @@ public abstract class Employee{
         return String.format("%s, %s: %s - %s", lastName, firstName,moneyFormat.format(getSalary()), moneyFormat.format(getBonus()));
     }
 
-    private static final class DummyEmployee extends Employee{
+    private static final class DummyEmployee extends Employee implements IEmployee{
         @Override
         public int getSalary() {
             return 0;
