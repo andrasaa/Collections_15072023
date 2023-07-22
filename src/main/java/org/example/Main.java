fb1bd6f6621dd717aa5e9e020530a1eb30ec8fc7
Andras Andras <andrasx2@gmail.com>
2023-07-22 21:26:45 +0100

    Commit_150. Intro to Sets

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.sql.Array;
 import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.util.*;
@@ -9,8 +10,14 @@ public class Main {
 
     public static void main(String[] args) {
         String peopleText = """
-            Flinstone1, Fred1, 1/1/1900, Programmer, {locpd=2000,yoe=10,iq=140}
-            Flinstone2, Fred2, 1/1/1900, Programmerzzzz, {locpd=1300,yoe=14,iq=100}
+            Flinstone, Fred, 1/1/1900, Programmer, {locpd=2000,yoe=10,iq=140}
+            Flinstone, Fred, 1/1/1900, Programmer, {locpd=2000,yoe=10,iq=140}
+            Flinstone, Fred, 1/1/1900, Programmer, {locpd=2000,yoe=10,iq=140}
+            Flinstone, Fred, 1/1/1900, Programmer, {locpd=2000,yoe=10,iq=140}
+            Flinstone, Fred, 1/1/1900, Programmer, {locpd=2000,yoe=10,iq=140}
+            Flinstone, Fred, 1/1/1900, Programmer, {locpd=2000,yoe=10,iq=140}
+            Flinstone, Fred, 1/1/1900, Programmerzzzz, {locpd=2000,yoe=10,iq=140}
+            Flinstone2, Fred2, 1/1/1900, Programmer, {locpd=1300,yoe=14,iq=100}
             Flinstone3, Fred3, 1/1/1900, Programmer, {locpd=2300,yoe=8,iq=105}
             Flinstone4, Fred4, 1/1/1900, Programmer, {locpd=1630,yoe=3,iq=115}
             Flinstone5, Fred5, 1/1/1900, Programmer, {locpd=5,yoe=10,iq=100}
@@ -32,71 +39,20 @@ public class Main {
 
         int totalSalaries = 0;
         IEmployee employee = null;
-        List<IEmployee> employees = new LinkedList<>();
+        Set<IEmployee> employees = new HashSet<>();
         while (peopleMat.find()) {
             employee = Employee.createEmployee(peopleMat.group());
             employees.add(employee);
-            //Changed:
+          }
 
-//            if (employee instanceof Programmer prog){
-////                Programmer prog = (Programmer) employee;
-//                System.out.println(prog.getIq());
-//            } else if (employee instanceof Manager man){
-//                System.out.println(man.toString());
-//            } else if (employee instanceof Analyst){
-//                System.out.println();
-//            } else if (employee instanceof Ceo){
-//                System.out.println();
-//            } else {
-//                System.out.println("Default output");
-//            }
-//            System.out.println(employee.toString());
-//            totalSalaries+= employee.getSalary();
-        }
-
-
-
-        IEmployee myEmp = employees.get(5);
-        System.out.println(employees.contains(myEmp));
-
-        IEmployee employee1 = Employee.createEmployee("Flinstone5, Fred5, 1/1/1900, Programmer, {locpd=5,yoe=10,iq=100}");
-        System.out.println(employees.contains(employee1));
-
-        System.out.println(myEmp.equals(employee1));
-
-        Collections.shuffle();
-        Collections.sort(employees, Comparator.reverseOrder());
-
-//        employees.sort((o1, o2) -> {
-//            if (o1 instanceof Employee emp1 && o2 instanceof Employee emp2){
-//                int lnameResult =  emp1.lastName.compareTo(emp2.lastName);
-//                return lnameResult != 0 ? lnameResult : Integer.compare(emp1.getSalary(),emp2.getSalary());
-//            }
-//        return 0;
-//        });
-
-//        employees.set(0, Employee.createEmployee("Rubble, Betty, 4/4/1915, CEO, {avgStockPrice=300)"));
-//
-//        List<String> undesirables = new ArrayList<>(List.of("Wilma5","Barney4","Fred2"));
-//        undesirables.sort(Comparator.naturalOrder());
-//        System.out.println(undesirables);
-        //        removeUndesirables(employees, undesirables);
-//
-//        List<String> newStrings = new ArrayList<>();
-//        newStrings.addAll(undesirables);
-//        System.out.println(newStrings.size());
-//
         for(IEmployee worker : employees){
             System.out.println(worker.toString());
             totalSalaries += worker.getSalary();
         }
 
-//        NumberFormat currencyInstance = NumberFormat.getCurrencyInstance();
-//        System.out.printf("The total payout should be %s%n", currencyInstance.format(totalSalaries));
-//
-//    Weirdo larry = new Weirdo("David", "Larry", LocalDate.of(1950,1,1));
-//    Weirdo jake = new Weirdo("Snake","Jake");
-//    jake.sayHello();
+        NumberFormat currencyInstance = NumberFormat.getCurrencyInstance();
+        System.out.printf("The total payout should be %s%n", currencyInstance.format(totalSalaries));
+        System.out.println(employees.size());
 }
 
     private static void removeUndesirables(List<IEmployee> employees, List<String> removalNames) {
